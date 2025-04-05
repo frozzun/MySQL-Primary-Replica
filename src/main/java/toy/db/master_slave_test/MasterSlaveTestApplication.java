3f39c208651e969644b09e2de7ff32f5387e70e9
frozzun <eonjun0783@naver.com>
2025-04-05 22:03:29 +0900

    Initial commit

---
+++ src/main/java/toy/db/master_slave_test/MasterSlaveTestApplication.java
@@ -0,0 +1,13 @@
+package toy.db.master_slave_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSlaveTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSlaveTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/db/master_slave_test/MasterSlaveTestApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.db.master_slave_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSlaveTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
